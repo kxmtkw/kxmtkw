93f5c19a47a95a5e4dc602070a9700f2aaaede19
haseeb <haseeeb8j@gmail.com>
2026-07-07 23:22:48 +0500

    Manual

--- generator.py
+++ generator.py
@@ -53,7 +53,7 @@ def contactInfo() -> dict[str, str]:
 
 def makeReadme(content: str, svg: str, readme: str):
 		
-	formatter = SvgFormatter(style="monokai", font_family="iosevka", font_size=12, line_height=1)
+	formatter = SvgFormatter(style="github-dark", font_family="iosevka", font_size=12, line_height=1)
 
 	with open(svg, "w") as f:
 		highlight(content, PythonLexer(), formatter, f)
