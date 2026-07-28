0f0c87ce4cde0eec6219aa665fe968bdfaa8d208
haseeb <haseeeb8j@gmail.com>
2026-07-28 22:56:54 +0500

    Manual

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import generator
 
 age = generator.age()
 location = generator.location()
-hobbies   = [str(h) for h in generator.hobbies()]
-expertise = [str(e) for e in generator.expertise()]
+hobbies   = [f"- {str(h)}" for h in generator.hobbies()]
+expertise = [f"- {str(e)}" for e in generator.expertise()]
 techs     = [str(t) for t in generator.technologies()]
 
 
