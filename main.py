7bf964c775a20eecc78c4369f772d09f8e8a5cfb
haseeb <haseeeb8j@gmail.com>
2026-07-28 23:21:56 +0500

    Manual

--- main.py
+++ main.py
@@ -6,37 +6,34 @@ expertise = generator.expertise()
 techs = generator.technologies()
 contacts = generator.contactInfo()
 
-fmt_hobbies = "\n".join(f"- {h}" for h in hobbies)
-fmt_expertise = "\n".join(f"- {e}" for e in expertise)
-fmt_techs = "\n".join(f"- {t}" for t in techs)
+fmt_hobbies = "\n".join(f"  - {h}" for h in hobbies)
+fmt_expertise = "\n".join(f"  - {e}" for e in expertise)
+fmt_techs = "\n".join(f"  - {t}" for t in techs)
 
 longest_contact_key = max(len(key) for key in contacts.keys())
 fmt_contacts = "\n".join(
-    f"{key:<{longest_contact_key}} = \"{val}\"" for key, val in contacts.items()
+    f"  {key:<{longest_contact_key}} : {val}" for key, val in contacts.items()
 )
 
-readme = f"""
-[profile]
+readme = f"""# Profile
+profile:
+  name     : A. Haseeb Khalid
+  age      : {age}
+  location : Lahore, Pakistan
 
-name     = "A. Haseeb Khalid"
-age      = "{age}"
-location = "Lahore, Pakistan"
-
-hobbies = \"""
-{fmt_hobbies}
-\"""
-
-[skills]
-
-expertise = \"""
+# Focus & Skills
+expertise:
 {fmt_expertise}
-\"""
 
-technologies = \"""
+technologies:
 {fmt_techs}
-\"""
 
-[contacts]
+# Hobbies
+hobbies:
+{fmt_hobbies}
+
+# Communications
+contacts:
 {fmt_contacts}
 """
 
