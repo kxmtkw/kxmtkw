3ff2d615d6645ac359314a432e95b3faed9ab20a
haseeb <haseeeb8j@gmail.com>
2026-08-12 06:33:11 +0500

    Manual

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<img src="profile1.svg" width="400" height="720" alt="Github Profile">
+<img src="profile2.svg" width="400" height="660" alt="Github Profile">
--- generator.py
+++ generator.py
@@ -66,7 +66,7 @@ def makeReadme(content: str, svg: str, readme: str):
 	with open(svg, "w") as f:
 		highlight(content, YamlLexer(), formatter, f)
 		
-	height = content.count('\n') * 24
+	height = content.count('\n') * 22
 	width = max(len(h) for h in content.splitlines()) * 10
      
 	readme_content = f'<img src="{svg}" width="{width}" height="{height}" alt="Github Profile">'
